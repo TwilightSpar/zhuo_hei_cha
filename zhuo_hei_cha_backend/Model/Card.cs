fd388ebd28baf2784e5b4acd4a1dcdfb45821d89
Bill Zhang <bill.nonbusiness@gmail.com>
2020-04-28 12:08:55 -0400

    sample refactor for Card class

--- zhuo_hei_cha_backend/Model/Card.cs
+++ zhuo_hei_cha_backend/Model/Card.cs
@@ -1,41 +1,49 @@
 using System;
-using System.Collections.Generic;
 
 // understand the attribute of a card
-public class Card{
-    private readonly int _id;
-    private int number;
-
-    private void InitNumber(){      // A is 14, 2 is 15
-        if(_id>51)
-        number = _id%13;
-    else 
-        number = _id%13+3;
-    }
-    
-    
+public class Card
+{
+    // max card id (inclusive)
     public static readonly int CARD_MAX = 53;
 
-    public Card(int cardNumber)
-    {        
-        if(cardNumber<0 || cardNumber>CARD_MAX)
-            throw new Exception("cardNumber is invalid");
-        this._id = cardNumber;
+    // max non-Joker card id (inclusive)
+    public static readonly int NON_JOKER_CARD_MAX = 51;
 
-        InitNumber();
+    // number of cards in a suit
+    public static readonly int CARDS_PER_SUIT = 13;
+
+    // offset applied to all non-Joker cards when calculating the Number property
+    public static readonly int CARD_NUMBER_OFFSET = 3;
+
+    private readonly int _id;
+
+    public Card(int id)
+    {        
+        if (id < 0 || id > CARD_MAX)
+            throw new ArgumentOutOfRangeException("cardNumber is invalid");
+        
+        this._id = id;
     }
 
     public Suit Suit
     {
-        get{return (Suit)(_id/13);}        
+        get
+        {
+            return (Suit)(_id / CARDS_PER_SUIT);
+        }
     }
-
-    // jokers' number are 0 and 1
+    
     public int Number
     {
-        get{
-            return number;
+        get
+        {
+            if (_id <= NON_JOKER_CARD_MAX)
+            {
+                return _id % CARDS_PER_SUIT + CARD_NUMBER_OFFSET;
+            }
+            
+            // jokers' number are 0 and 1
+            return _id % CARDS_PER_SUIT;
         }
-        set{ number += value;}
     }
 }
